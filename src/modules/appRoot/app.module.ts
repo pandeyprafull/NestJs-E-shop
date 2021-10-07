e9757517d6acebd236d74437b69effd241304283
pandeyprafull <prafullpandey68@gmail.com>
2021-10-07 18:02:24 +0530

    prepare basic setup

--- src/modules/appRoot/app.module.ts
+++ src/modules/appRoot/app.module.ts
@@ -7,6 +7,7 @@
  import { AppController } from './app.controller';
  import { AppService } from './app.service';
  import { HttpModule } from '@nestjs/axios';
+ import { getEntities } from '@shop_org/schemas'
 
 //  import * as modules from '../index';
 
@@ -15,13 +16,28 @@
  @Global()
  @Module({
    imports: [
-
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URI,
+      // ssl: { rejectUnauthorized: false },
+      synchronize: process.env?.LOCAL === 'true' ?  true : false,
+      logging: process.env?.LOCAL === 'true' ? ['error', 'warn', 'query'] : false,
+      entities: [
+        ...getEntities()
+      ],
+      autoLoadEntities: true
+    }),
+    TypeOrmModule.forFeature(getEntities()),
      HttpModule,
     //  ...Object.values(modules)
    ],
    controllers: [AppController],
    providers: [AppService],
-   exports: []
+   exports: [
+     TypeOrmModule,
+     AppService,
+     HttpModule
+   ]
  })
  export class AppModule {}
 
